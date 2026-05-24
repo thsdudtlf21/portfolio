5c552278aa403d6e1488cb1d2fc9096666fb6f76
thsdudtlf21 <thsdudtlf21@gmail.com>
2026-05-24 14:28:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -107,17 +107,13 @@ window.PORTFOLIO_DATA = {
   inform: {
     aboutTitle: 'About Son yeongsil',
     about: '편집·그래픽·전시 디자인을 기반으로 다양한 매체에서 브랜드와 이벤트의 메시지를 효과적으로 전달하는 비주얼 커뮤니케이션 디자이너입니다.
-    </div>
     고객과 사용자 경험을 고려한 디자인을 지향하며, 직관적이고 인상적인 시각 커뮤니케이션을 추구합니다.
-    </div>
-    또한 미술교육 경험을 바탕으로 디자인과 교육을 연결하며, 참여자들이 직접 경험하고 몰입할 수 있는 시각 환경을 설계합니다.
-',
-    designTeam: ['Ban Yunjung | Art Director', 'Oh Hyuntaek | Designer', 'Seo Kanghyeon | Designer'],
-    partner: ['Seong Hayoung | Editor', 'Lee Eunsook | Photographer'],
-    management: ['Kim Seil | General Manager'],
-    contact: ['Tel. 02-765-9987', 'Fax. 02-765-9986', 'Mail. hongdan201@naver.com'],
-    location: ['경기 고양시 덕양구 으뜸로 8', '덕은아이에스비즈타워센트럴 1차 2110호', '(10542)'],
-    web: ['Hongdan201', 'Instagram']
+    또한 미술교육 경험을 바탕으로 디자인과 교육을 연결하며, 참여자들이 직접 경험하고 몰입할 수 있는 시각 환경을 설계합니다.',
+    Education: ['창원대학교 미술학과 한국화전공 졸업(2021-2025']
+    Experience: ['서울옥션 디자인팀 사원(2025.11-2026.05)', '엘리스 디자인팀 사원(2025.02-2025.10)',  '앤유 디자인팀 사원(2022.10-2024.02)']
+    contact: ['Tel. 010-3260-4380', 'Mail. thsdudtlf21@naver.com'],
+    location: ['Seoul/Busan'],
+    web: ['www.sys', 'Instagram']
   }
 };
 
